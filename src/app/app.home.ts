0371325cfc47a31a9eaf3a52d9991f714d130e9e
Alexandre Clark <alex.clark.015@gmail.com>
2018-02-24 15:41:15 -0500

    Initial commit

---
+++ src/app/app.home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './app.home.html',
+  styleUrls: ['./app.home.css']
+})
+export class HomeComponent {
+
+}
---
+++ src/app/app.products.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './app.products.html',
+  styleUrls: ['./app.products.css']
+})
+export class ProductsComponent {
+
+}
