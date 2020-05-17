4b09a811f6fb34c33a4a5ef26b529072d7a6977d
Alexandre Clark <alexandre.c@shopify.com>
2020-05-16 22:41:07 -0400

    Page des services

--- pages/services.tsx
+++ pages/services.tsx
@@ -1,8 +1,24 @@
+import path from 'path';
+
+import {GetStaticProps} from 'next';
 import Head from 'next/head';
 
-import {Hero} from '../components';
+import {
+  Hero,
+  ContentBlock,
+  Layout,
+  Inset,
+  BlockLayout,
+  Image,
+} from '../components';
+import {readMarkdownFilesFromDir} from '../utilities/markdown';
+import {Service} from '../utilities/types';
 
-export default function Services() {
+export interface Props {
+  services: Service[];
+}
+
+export default function Services({services}: Props) {
   return (
     <>
       <Head>
@@ -11,7 +27,40 @@ export default function Services() {
 
       <main>
         <Hero source="/images/tamiseur.jpg" title={['Nos', 'services']} />
+
+        <Layout>
+          <Inset>
+            <BlockLayout
+              style={{display: 'flex', flexWrap: 'wrap', overflow: 'hidden'}}
+            >
+              {services.map((service) => (
+                <ContentBlock
+                  key={service.title}
+                  title={service.title}
+                  style={{
+                    width: 300,
+                    flexGrow: 1,
+                    boxShadow: '0 0 0 1px #e5e5e5',
+                    background: 'white',
+                  }}
+                  items={service.items}
+                  unsafe={service.unsafe}
+                  subdued={service.subdued}
+                >
+                  {service.description}
+                </ContentBlock>
+              ))}
+            </BlockLayout>
+          </Inset>
+        </Layout>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps<Props> = async (_context) => {
+  const root = path.resolve(process.cwd());
+  const servicesRoot = path.join(root, 'content', 'services');
+  const services = await readMarkdownFilesFromDir<Service>(servicesRoot);
+  return {props: {services}};
+};
